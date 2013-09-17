d0bb13f4c1e10e3c206398314b24a2274ec670f7
Moshe Kamensky <kamensky@cpan.org>
2013-09-17 13:12:50 +0300

    initial commit

---
+++ lib/List/Sorted.pm
@@ -0,0 +1,19 @@
+# Created: Tue Sep 17 13:12:50 2013
+# Last Changed: TIMESTAMP
+
+package List::Sorted;
+use 5.10.0;
+use warnings;
+use integer;
+use strict;
+use Carp;
+use autodie qw(:all);
+
+1;
+
+__END__
+
+=head1 BUGS
+
+=cut
+
